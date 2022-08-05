2766e9d1112b9cee38072cfcee7119f1226741fa
AmelinRoman <94740458+AmelinRoman@users.noreply.github.com>
2022-08-05 11:09:10 +0300

    Initial Commit

---
+++ becomeAMillionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  becomeAMillionaire
+//
+//  Created by Macbook Pro on 05.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
