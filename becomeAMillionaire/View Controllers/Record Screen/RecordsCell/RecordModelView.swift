314d595ac379f2d90d0ac858ffa0a1026fef84aa
AmelinRoman <94740458+AmelinRoman@users.noreply.github.com>
2022-08-12 12:28:25 +0300

    Добавление комментариев

---
+++ becomeAMillionaire/View Controllers/Record Screen/RecordsCell/RecordModelView.swift
@@ -0,0 +1,16 @@
+//
+//  ModelView.swift
+//  becomeAMillionaire
+//
+//  Created by Macbook Pro on 11.08.2022.
+//
+
+import UIKit
+
+struct RecordViewModel {
+    var modelNameLabel: String
+    var modelDateLabel: String
+    var modelCountHint: String
+    var modelScore: String
+}
+
